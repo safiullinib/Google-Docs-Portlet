d943833418ae06782690023e0b3974edf795517c
Drew Blessing <drew.blessing@esu10.org>
2012-02-03 10:51:06 -0600

    Initial commit

--- docroot/WEB-INF/src/org/gnenc/googledocs/portlet/GoogleDocs.java
+++ docroot/WEB-INF/src/org/gnenc/googledocs/portlet/GoogleDocs.java
@@ -1,21 +1,21 @@
 /**
-*  Copyright (c) 2010-2011 Educational Service Unit 10. 
-*
-*  This file is part of the Google Docs portlet.
-*  
-*  Google Docs portlet is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-* 
-*  My Courses is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with the Google Docs portlet.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ *  Copyright (c) 2011-2012 Educational Service Unit 10. 
+ *
+ *  This file is part of the Google Docs portlet.
+ *  
+ *  Google Docs portlet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Google Docs portlet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Google Docs portlet.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 package org.gnenc.googledocs.portlet;
 
@@ -45,69 +45,65 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
 import com.liferay.util.portlet.PortletProps;
 
 public class GoogleDocs extends MVCPortlet {
-	public static DocumentListFeed getDocumentListFeed(
-			RenderRequest request) throws IOException, 
-			ServiceException, OAuthException, 
-			ParseException 
-	{
+	public static DocumentListFeed getDocumentListFeed(RenderRequest request)
+			throws IOException, ServiceException, OAuthException,
+			ParseException {
 		DocumentListFeed feed = new DocumentListFeed();
-		ThemeDisplay themeDisplay = 
-				(ThemeDisplay) request.getAttribute(
-						WebKeys.THEME_DISPLAY);
+		ThemeDisplay themeDisplay = (ThemeDisplay) request
+				.getAttribute(WebKeys.THEME_DISPLAY);
 		String userEmail = themeDisplay.getUser().getEmailAddress();
 		Boolean isDefaultUser = themeDisplay.getUser().isDefaultUser();
-		
+
 		if (!isDefaultUser) {
-			String consumerKey = GetterUtil.getString(
-				PortletProps.get(
-					"google.docs.oauth.consumer.key"));
-			String consumerSecret = GetterUtil.getString(
-				PortletProps.get(
-					"google.docs.oauth.consumer.secret"));
+			String consumerKey = GetterUtil.getString(PortletProps
+					.get("google.docs.oauth.consumer.key"));
+			String consumerSecret = GetterUtil.getString(PortletProps
+					.get("google.docs.oauth.consumer.secret"));
 			String scope = "https://docs.google.com/feeds/";
-			String feedUrl =  URLEncoder.encode(
-				userEmail,"UTF-8") + "/private/full";
+			String feedUrl = URLEncoder.encode(userEmail, "UTF-8")
+					+ "/private/full";
 			String requestor = URLEncoder.encode(
-				GetterUtil.getString(
-					PortletProps.get(
-						"google.docs.oauth.requestor")),"UTF-8");
-			
+					GetterUtil.getString(PortletProps
+							.get("google.docs.oauth.requestor")), "UTF-8");
+
 			GoogleOAuthParameters oauthParams = new GoogleOAuthParameters();
 			oauthParams.setOAuthConsumerKey(consumerKey);
 			oauthParams.setOAuthConsumerSecret(consumerSecret);
 			oauthParams.setScope(scope);
-			
+
 			OAuthSigner signer = new OAuthHmacSha1Signer();
-			
-			URL docListFeedUrl = new URL(
-				scope + feedUrl	+ "?xoauth_requestor_id=" + requestor);
+
+			URL docListFeedUrl = new URL(scope + feedUrl
+					+ "?xoauth_requestor_id=" + requestor);
 			System.out.println(docListFeedUrl);
 			DocsService service = new DocsService("Document List Demo");
 			service.setOAuthCredentials(oauthParams, signer);
-		
+
 			try {
-				feed = service.getFeed(docListFeedUrl,DocumentListFeed.class);
+				feed = service.getFeed(docListFeedUrl, DocumentListFeed.class);
 			} catch (ResourceNotFoundException e) {
 				feed = null;
-				SessionErrors.add(request, "this-is-not-a-valid-google-account");
+				SessionErrors
+						.add(request, "this-is-not-a-valid-google-account");
 			}
 		} else {
 			feed = null;
-			SessionErrors.add(request, "you-must-be-logged-in-to-view-google-docs");
+			SessionErrors.add(request,
+					"you-must-be-logged-in-to-view-google-docs");
 		}
 		return feed;
-		
+
 	}
-	
+
 	public static List<DocumentListEntry> getDocumentList(DocumentListFeed feed) {
 		List<DocumentListEntry> docList = new ArrayList<DocumentListEntry>();
-		
+
 		for (DocumentListEntry entry : feed.getEntries()) {
 			docList.add(entry);
-			
+
 		}
 		return docList;
-		
+
 	}
 
 }
--- docroot/init.jsp
+++ docroot/init.jsp
@@ -1,22 +1,22 @@
 <%
 /**
-*  Copyright (c) 2010-2011 Educational Service Unit 10. 
-*
-*  This file is part of the My Courses portlet.
-*  
-*  My Courses portlet is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, either version 3 of the License, or
-*  (at your option) any later version.
-* 
-*  My Courses is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with the My Courses portlet.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ *  Copyright (c) 2011-2012 Educational Service Unit 10. 
+ *
+ *  This file is part of the Google Docs portlet.
+ *  
+ *  Google Docs portlet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Google Docs portlet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Google Docs portlet.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 %>
 
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
